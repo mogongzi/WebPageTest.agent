efb3113776d836603bcc716167869851d2e064b0
Ryan.Ren <mogongzi@sina.cn>
2025-02-11 10:47:55 +0800

    Update entrypoint.sh

--- docker/linux-headless/entrypoint.sh
+++ docker/linux-headless/entrypoint.sh
@@ -27,6 +27,10 @@ if [ -n "$SHAPER" ]; then
   EXTRA_ARGS="$EXTRA_ARGS --shaper $SHAPER"
 fi
 
+echo $EXTRA_ARGS
+echo $SERVER_URL
+echo $LOCATION
+
 # exec replaces the shell process by the python process and is required to
 # propagate signals (i.e. SIGTERM)
 exec python3 /WebPageTest.agent/wptagent.py --server "${SERVER_URL}" --location "${LOCATION}" ${EXTRA_ARGS} --xvfb --dockerized -vvvv "@"
